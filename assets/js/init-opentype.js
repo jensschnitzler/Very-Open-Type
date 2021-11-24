7d6940a825daa3e371a827c15ccb94ba3b9a648d
jensschnitzler <jens.schnitzler@gmail.com>
2021-11-24 14:51:37 +0100

    update

--- assets/js/init-opentype.js
+++ assets/js/init-opentype.js
@@ -1,4 +1,4 @@
-console.log('hello');
+console.log('init-opentype.js');
 
 opentype.load('/Very-Open-Type/typefaces/Trace/fonts/Trace-Regular.otf', function(err, font) {
   if (err) {
@@ -6,4 +6,24 @@ opentype.load('/Very-Open-Type/typefaces/Trace/fonts/Trace-Regular.otf', functio
   } else {
     console.log({font});
   }
+});
+
+// --- Functions
+
+function init_character_map(){
+  var containers = $('.character-map');
+  console.log({containers});
+  if(containers.length >  0){
+    console.log('init_character_map');
+    containers.each(function(){
+      var container = $(this);
+      container.html('I am a container!');
+    });
+  }
+}
+
+// --- Execute
+
+$(function() {
+  init_character_map()
 });
--- typefaces/Trace/index.md
+++ typefaces/Trace/index.md
@@ -1,1 +1,5 @@
+---
+layout: speciman
+---
+
 # Trace
