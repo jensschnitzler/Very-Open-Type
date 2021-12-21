d4ff5955ffe7ef0f50dce1b7adf88828a7f42b78
jensschnitzler <jens.schnitzler@gmail.com>
2021-12-21 15:10:15 +0100

    update

---
+++ assets/js/fontSizeSlider.js
@@ -0,0 +1,22 @@
+console.log('fontSizeSlider,js');
+
+function setSliderFontSize($slider){
+  var val = $slider.val() || 40;
+  var $wrapper = $slider.closest('.wrapper');
+  var $editor = $wrapper.find('.editor').first();
+  $editor.css({
+    'fontSize':val + 'px',
+  });
+}
+
+$(function(){
+  var $body = $('body');
+  var $sliders = body.find('.fontSizeSlider');
+  $sliders.each(function(){
+    var $slider = $(this);
+    setSliderFontSize($slider);
+    $slider.change(function(){
+      setSliderFontSize($slider);
+    });
+  });
+});
